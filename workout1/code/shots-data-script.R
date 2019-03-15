511e3e509872da0cb6bdbd5fdf602da679334067
Kimberly Chua <k.chua0514@berkeley.edu>
2019-03-15 02:45:54 -0700

    R file update

--- workout1/code/shots-data-script.R
+++ workout1/code/shots-data-script.R
@@ -7,3 +7,14 @@
 ## Date: 3/13/2019
 ## Author: kimberly chua
 ##################################################
+
+curry <- read.csv(
+  "data/stephen-curry.csv", stringsAsFactors = FALSE)
+igudala <- read.csv(
+  "data/andre-iguodala.csv", stringsAsFactors = FALSE)
+green <- read.csv(
+  "data/draymond-green.csv", stringsAsFactors = FALSE)
+durant <- read.csv(
+  "data/kevin-durant.csv", stringsAsFactors = FALSE)
+thompson <- read.csv(
+  "data/klay-thompson.csv", stringsAsFactors = FALSE)
